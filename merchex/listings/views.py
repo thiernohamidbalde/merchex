84f23fa06a1b884b0abe5a5871d56d5128b0f12c
thiernohamidbalde <thiernodjobalde@icloud.com>
2023-07-22 13:44:44 +0000

    creation des templates, gabarit pour le html, faire la differenceentre  la vue et la presentation des pages

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,27 +6,24 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""<h1>Hello django</h1>
-                         <p> mes groupes preferes sont : </p>
-                         <ul>
-                           <li>{bands[0].name}</li>
-                           <li>{bands[1].name}</li>
-                           <li>{bands[2].name}</li>
-                         </ul>
-""")
+    return render(request, 'listings/hello.html',
+                  context={'bands': bands})
+   # return HttpResponse(f"""<h1>Hello django</h1>
+    #                     <p> mes groupes preferes sont : </p>
+     #                    <ul>
+       #                    <li>{bands[0].name}</li>
+        #                   <li>{bands[1].name}</li>
+        #                   <li>{bands[2].name}</li>
+         #                </ul>
+#""")
 
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p> nous adorons merchex</p>')
 
 def listings(request):
     listing = Listing.objects.all()
-    return HttpResponse(f"""<h1>Liste des sons</h1> <p> Vos titres sont : </p>
-    <ul>
-        <li>{listing[0].title}</li>
-        <li>{listing[1].title}</li>
-        <li>{listing[2].title}</li>
-        <li>{listing[3].title}</li>
-""")
+    return render(request, 'listings/listings.html',
+                  context={'listing': listing})
 
 def contact(request):
     return HttpResponse('<h1>Nous contacter</h1> <p>votre message</p>')
