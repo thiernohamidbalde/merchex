b45b76cd61c0de5119ed66d8d0b19290244a5e50
thiernohamidbalde <thiernodjobalde@icloud.com>
2023-08-08 12:59:16 +0000

    ajout des des fichier du 3e chap du cours debuter avec django sur openclassroom

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+#from bands.models import Band
+
 # Register your models here.
+
+from listings.models import Band, Listing
+
+class BandAdmin(admin.ModelAdmin): # nous inserons lignes...
+    list_display = ('name', 'year_formed', 'genre') # listes des champs que nous voulons sur l'affichage de la liste
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sold', 'year')
+
+
+admin.site.register(Band, BandAdmin) # nous modifions cette ligne en ajoutant un 2e argument
+admin.site.register(Listing, ListAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,9 +1,44 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Band(models.Model):
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Type(models.TextChoices):
+        RECORD = 'RC'
+        CLOTHING = 'CL'
+        POSTERS = 'PO'
+
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=10)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+    #like_new = models.CharField(null=True, blank=True, max_length=10)
