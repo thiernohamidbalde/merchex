3f12d1ed88fa786b50edb3930f2f6656059f6cf7
thiernohamidbalde <thiernodjobalde@icloud.com>
2023-08-11 19:49:57 +0000

    premier ajout des fichiers du 1er chap de la derniere partie du cours

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html',
+    return render(request, 'listings/band_list.html',
                   context={'bands': bands})
+
+def band_detail(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request, 'listings/band_detail.html',
+                  {'band': band})
    # return HttpResponse(f"""<h1>Hello django</h1>
     #                     <p> mes groupes preferes sont : </p>
      #                    <ul>
@@ -25,6 +30,11 @@ def listings(request):
     listing = Listing.objects.all()
     return render(request, 'listings/listings.html',
                   context={'listing': listing})
+
+def listing_detail(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    return render(request, 'listings/listing_detail.html',
+                  {'listing': listing})
 
 def contact(request):
     #return HttpResponse('<h1>Nous contacter</h1> <p>votre message</p>')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,8 +20,14 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+
+    path('bands/', views.band_list, name="band-list"),
+    path("bands/<int:band_id>/", views.band_detail, name="band-detail"),
+
     path('about-us/', views.about),
-    path('listings/', views.listings),
+
+    path('listings/', views.listings, name="listing-list"),
+    path("listings/<int:listing_id>/", views.listing_detail, name="listing-detail"),
+
     path('contact-us/', views.contact),
 ]
